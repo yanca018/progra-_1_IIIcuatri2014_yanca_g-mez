2b6e32a11e8c835591ac93c6decc9b2aa7435adf
yanca018 <yanca0118@hotmail.com>
2014-09-27 13:30:07 -0700

    clase 3

---
+++ clases/CLASE3/src/Ejercicio1.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yanca
+ */ //escribir un programa que solicite la carga de un valor postivo y nos muestre desde 1 hasta el valor ingresado de uno en un ejemplo: si ingresamos 30 se debe mostrar en pantalla los numeros del 1 al 30
+
+public class Ejercicio1 {
+    
+    private String numero="";
+
+    public Ejercicio1(int numero) {
+        
+        this.ImprimirNumeros(numero);
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    
+    private String ImprimirNumeros(int numero){
+    
+    for(int i=0; i<=10; i++){
+        numero +=""+i+ getNumero();{
+    
+    
+    
+    }
+    return getNumero();
+    
+    
+    }
+    public String getNumero(){
+     return numero;
+    
+    }
+    
+    }
+    
+    
+  
+    
+}
+
+
